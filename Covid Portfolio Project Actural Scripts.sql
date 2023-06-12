6c5666d03d2ba3e888eb2a2f8f40b5b8750dd65c
CellaShang <135455467+CellaShang@users.noreply.github.com>
2023-06-11 23:04:54 -0400

    Add files via upload

---
+++ Covid Portfolio Project Actural Scripts.sql
@@ -0,0 +1,93 @@
+
+--Look at Total cases VS Total deaths
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM dbo.CovidDeaths
+WHERE location LIKE '%states%'
+ORDER BY 1,2;
+
+--Look at Countries with Highest Infection Rate and Population
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population)*100) AS PercentagePopulationInfected
+FROM dbo.CovidDeaths
+GROUP BY location, population
+ORDER BY PercentagePopulationInfected DESC;
+
+--Look at Countries with Highest Death Count per Population
+SELECT location, MAX(total_deaths) AS TotalDeathCount
+FROM dbo.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY location
+ORDER BY TotalDeathCount DESC;
+
+--Look at Countinent with Highest Death Count per population
+SELECT continent, MAX(total_deaths) AS TotalDeathCount
+FROM dbo.CovidDeaths
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC;
+
+
+--Global numbers
+SELECT date, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, SUM(new_deaths)/NULLIF(SUM(new_cases),0)*100 AS DeathPercentage
+FROM dbo.CovidDeaths
+GROUP BY date	
+ORDER BY 1,2;
+
+--Look at Total Population VS Vaccination
+SELECT dea.location, dea.date, dea.continent, dea.continent, vac.new_vaccinations
+FROM dbo.CovidDeaths AS dea
+JOIN dbo.CovidVaccinations AS vac
+ON dea.location = vac.location
+AND dea.date = vac.date	
+WHERE dea.continent IS NOT NULL
+ORDER BY 2,3;
+
+-- USE CTE
+WITH PopvsVac
+AS 
+(
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM dbo.CovidDeaths dea
+JOIN dbo.CovidVaccinations vac
+ON dea.location = vac.location
+AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+)
+SELECT *, RollingPeopleVaccinated/population
+FROM PopvsVac
+
+--Temp Table
+DROP Table IF EXISTS  #PercentPopulationVaccinated
+CREATE TABLE #PercentPopulationVaccinated
+(
+Continent nvarchar(225),
+Location nvarchar(225),
+date datetime,
+Population numeric,
+New_vaccination numeric,
+RollingPeopleVaccinated numeric
+)
+
+INSERT INTO #PercentPopulationVaccinated
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM dbo.CovidDeaths dea
+JOIN dbo.CovidVaccinations vac
+ON dea.location = vac.location
+AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+
+SELECT *, RollingPeopleVaccinated/population
+FROM #PercentPopulationVaccinated
+
+CREATE VIEW PercentPopulationVaccinated AS
+SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
+SUM(CONVERT(bigint,vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM dbo.CovidDeaths dea
+JOIN dbo.CovidVaccinations vac
+ON dea.location = vac.location
+AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+
+SELECT * 
+FROM PercentPopulationVaccinated
